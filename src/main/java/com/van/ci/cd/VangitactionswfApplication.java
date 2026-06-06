7091783141c60058bcd3c7ff311a3218cbb47a1f
localkart1 <localkart3@gmail.com>
2026-06-06 16:29:52 +0530

    first commit

---
+++ src/main/java/com/van/ci/cd/VangitactionswfApplication.java
@@ -0,0 +1,31 @@
+package com.van.ci.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VangitactionswfApplication {
+
+	@GetMapping("/testing")
+	public String testing() {
+		System.out.print("welcome to chennai...");
+		return "Welcome to Java techie world";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(VangitactionswfApplication.class, args);
+	}
+
+}
+
+//
+//echo "# vanactions" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/techinfo1891/vanactions.git
+//git push -u origin main
