80168a26b04bcdf39c6c1f5bff4e24156228f883
techinfo1891 <techinfo1891@gmail.com>
2026-06-06 17:15:59 +0530

    second
    commit

--- src/main/java/com/van/ci/cd/VangitactionswfApplication.java
+++ src/main/java/com/van/ci/cd/VangitactionswfApplication.java
@@ -11,7 +11,7 @@ public class VangitactionswfApplication {
 
 	@GetMapping("/testing")
 	public String testing() {
-		System.out.print("welcome to chennai........");
+		System.out.print("welcome to chennai.......>>><<<<<.........");
 		return "Welcome to Java techie world";
 	}
 	
